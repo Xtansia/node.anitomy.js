8c86482d53025782c5d511a308c331342ba83a4f
Thomas Farr <xtansia@xtansia.com>
2016-06-14 05:26:54 +1200

    Add helper definitions for ensuring type of JS value, and defining and setting accessors

--- src/util.h
+++ src/util.h
@@ -27,15 +27,31 @@ NAN_SETTER(READ_ONLY_SETTER);
 #define NODE_LOCAL_WSTRING(str) \
   Nan::New(WstrToStr(str)).ToLocalChecked()
 
+#define NODE_ENSURE_VALUE_BOOLEAN(name, value) \
+  if (!value->IsBoolean()) { \
+    Nan::ThrowTypeError(#name " must be a boolean"); \
+    return; \
+  }
+
+#define NODE_ENSURE_VALUE_STRING(name, value) \
+  if (!value->IsString()) { \
+    Nan::ThrowTypeError(#name " must be a string"); \
+    return; \
+  }
+
 #define NODE_STRING_ARG(i, name) \
   if (info.Length() < i + 1) { \
     Nan::ThrowError(#name " must be given"); \
     return; \
   } \
-  if (!info[i]->IsString()) { \
-    Nan::ThrowTypeError(#name " must be a string"); \
-    return; \
-  } \
+  NODE_ENSURE_VALUE_STRING(name, info[i]); \
   std::wstring name = StrToWstr(*Nan::Utf8String(info[i]));
+
+#define NODE_GETTER_AND_SETTER(name) \
+  static NAN_GETTER(Get##name); \
+  static NAN_SETTER(Set##name);
+
+#define NODE_SET_ACCESSOR(jsName, cppName) \
+  Nan::SetAccessor(tpl->InstanceTemplate(), NODE_LOCAL_STRING(jsName), Get##cppName, Set##cppName);
 
 #endif // !ANITOMYJS_UTIL_H
