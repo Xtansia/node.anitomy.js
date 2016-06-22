2ec150298ca3c0fc98fd050a4d47b9dc3a9ccd24
Thomas Farr <xtansia@xtansia.com>
2016-06-23 04:53:44 +1200

    Start writing test cases

--- test/test.js
+++ test/test.js
@@ -6,4 +6,31 @@
 ** file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
-var anitomy = require('../anitomy');
+const expect = require('chai').expect;
+const anitomy = require('../anitomy');
+
+function parseSync(filename) {
+  return function () {
+    return anitomy.parseSync(filename);
+  }
+}
+
+describe('Anitomy', function () {
+  describe('#parseSync(filename)', function () {
+    it('should throw Error when filename is not provided', function () {
+      expect(parseSync()).to.throw(Error, 'filename must be provided');
+
+      expect(parseSync('')).to.not.throw(Error, 'filename must be provided');
+    });
+
+    it('should throw TypeError when filename is provided but isn\'t a string', function () {
+      expect(parseSync(null)).to.throw(TypeError, 'filename must be a string');
+      expect(parseSync(10)).to.throw(TypeError, 'filename must be a string');
+      expect(parseSync(false)).to.throw(TypeError, 'filename must be a string');
+      expect(parseSync([])).to.throw(TypeError, 'filename must be a string');
+      expect(parseSync({})).to.throw(TypeError, 'filename must be a string');
+
+      expect(parseSync('')).to.not.throw(TypeError, 'filename must be a string');
+    });
+  });
+});
