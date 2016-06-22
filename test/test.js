54ce096c5f6fa8e2ee616d05911dd8a7113449ec
Thomas Farr <xtansia@xtansia.com>
2016-06-23 00:40:22 +1200

    NPM init & empty JS files

---
+++ test/test.js
@@ -0,0 +1,9 @@
+/*
+** Copyright (c) 2016, Thomas Farr
+**
+** This Source Code Form is subject to the terms of the Mozilla Public
+** License, v. 2.0. If a copy of the MPL was not distributed with this
+** file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+
+var anitomy = require('../anitomy');
