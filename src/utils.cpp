80978f5dff06253741eec97cbe76edd78cc80e9a
Thomas Farr <xtansia@xtansia.com>
2016-11-21 18:48:57 +1300

    Rename config/macro for using boost library, don't define by default

--- src/utils.cpp
+++ src/utils.cpp
@@ -8,7 +8,7 @@
 
 #include "utils.h"
 
-#ifdef USE_BOOST_LOCALE_UTF_CONV
+#ifdef NODE_ANITOMY_USE_BOOST
   #include <boost/locale/encoding_utf.hpp>
 #else
   #include <codecvt>
@@ -17,13 +17,13 @@
 
 #include <string>
 
-#ifndef USE_BOOST_LOCALE_UTF_CONV
+#ifndef NODE_ANITOMY_USE_BOOST
   static std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t>
   wstringConverter;
 #endif
 
 std::string WstrToStr(const std::wstring &input) {
-  #ifdef USE_BOOST_LOCALE_UTF_CONV
+  #ifdef NODE_ANITOMY_USE_BOOST
   return boost::locale::conv::utf_to_utf<char>(input);
   #else
   return wstringConverter.to_bytes(input);
@@ -31,7 +31,7 @@ std::string WstrToStr(const std::wstring &input) {
 }
 
 std::wstring StrToWstr(const std::string &input) {
-  #ifdef USE_BOOST_LOCALE_UTF_CONV
+  #ifdef NODE_ANITOMY_USE_BOOST
   return boost::locale::conv::utf_to_utf<wchar_t>(input);
   #else
   return wstringConverter.from_bytes(input);
