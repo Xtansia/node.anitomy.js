e33a4fd0256790a16363bddc6a0c75864998efa2
Thomas Farr <xtansia@xtansia.com>
2016-11-21 20:22:22 +1300

    Use a try_compile to test for codecvt support, auto-enable boost-lib if not available

---
+++ compiler_tests/codecvt_test.cpp
@@ -0,0 +1,33 @@
+/*
+** Copyright (c) 2016, Thomas Farr
+**
+** This Source Code Form is subject to the terms of the Mozilla Public
+** License, v. 2.0. If a copy of the MPL was not distributed with this
+** file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+
+#include <cassert>
+#include <codecvt>
+#include <iostream>
+#include <locale>
+#include <string>
+
+static std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> wstringConverter;
+
+std::string WstrToStr(const std::wstring &input) {
+  return wstringConverter.to_bytes(input);
+}
+
+std::wstring StrToWstr(const std::string &input) {
+  return wstringConverter.from_bytes(input);
+}
+
+int main(void) {
+  std::locale::global(std::locale(""));
+  std::wcout.imbue(std::locale());
+  std::wstring wstr = L"こんいちは、i 8 Σ π";
+  std::string str = WstrToStr(wstr);
+  std::wstring wstr2 = StrToWstr(str);
+  assert(wstr == wstr2);
+  std::wcout << wstr << std::endl << wstr2 << std::endl;
+}
